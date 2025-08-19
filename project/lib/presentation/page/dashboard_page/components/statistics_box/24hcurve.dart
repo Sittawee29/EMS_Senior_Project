fd6a1e319a90c3031f3aa50912e9489eed11235e
Sittawee <Sittawee2004@gmail.com>
2025-08-19 17:21:41 +0700

    auto update

--- project/lib/presentation/page/dashboard_page/components/statistics_box/24hcurve.dart
+++ project/lib/presentation/page/dashboard_page/components/statistics_box/24hcurve.dart
@@ -105,15 +105,15 @@ class _LineChart extends StatelessWidget {
               top: BorderSide(color: Palette.mediumGrey40, width: 1),
             ),
           ),
-          lineBarsData: [
-            LineChartBarData(
-              isCurved: false,
-              color: Palette.lightBlue,
-              barWidth: 3,
-              spots: activeUsersData.entries
-                  .map((e) => FlSpot(e.key.toDouble(), e.value.first!))
-                  .toList(),
-            ),
+          LineChartBarData(
+  isCurved: false,
+  color: Palette.lightBlue,
+  barWidth: 3,
+  spots: activeUsersData.entries
+      .where((e) => e.value.first != null)
+      .map((e) => FlSpot(e.key.toDouble(), e.value.first!))
+      .toList(),
+),
             LineChartBarData(
               isCurved: false,
               color: Palette.orange,
