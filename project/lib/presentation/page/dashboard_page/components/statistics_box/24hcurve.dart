dae28fc854deb93a3697248c4136dee4397543bd
Sittawee <Sittawee2004@gmail.com>
2025-08-19 22:26:20 +0700

    auto update

--- project/lib/presentation/page/dashboard_page/components/statistics_box/24hcurve.dart
+++ project/lib/presentation/page/dashboard_page/components/statistics_box/24hcurve.dart
@@ -128,7 +128,7 @@ class _LineChart extends StatelessWidget {
             bottomTitles: AxisTitles(
               sideTitles: SideTitles(
                 showTitles: true,
-                reservedSize: 5,
+                reservedSize: 25,
                 interval: 2, // ทุก 30 นาที
                 getTitlesWidget: (value, meta) {
                   final index = value.toInt();
