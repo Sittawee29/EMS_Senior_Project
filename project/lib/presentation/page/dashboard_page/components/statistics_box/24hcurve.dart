1f3578e37a378d4144e42c2750cb921f614ab99b
Sittawee <Sittawee2004@gmail.com>
2025-08-19 19:01:51 +0700

    auto update

--- project/lib/presentation/page/dashboard_page/components/statistics_box/24hcurve.dart
+++ project/lib/presentation/page/dashboard_page/components/statistics_box/24hcurve.dart
@@ -63,7 +63,7 @@ class _LineChart extends StatelessWidget {
                   return LineTooltipItem(
                     '${lineNames[index]}: ${barSpot.y.toStringAsFixed(0)} kW',
                     TextStyle(
-                      fontSize: 16,
+                      fontSize: 4,
                       color: Colors.white,
                     ),
                   );
